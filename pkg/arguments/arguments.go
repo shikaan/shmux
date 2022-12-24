a1ae81df54937fdd8d5f2c1ae0a9a5b6157212a7
Manuel Spagnolo <spagnolo.manu@gmail.com>
2022-12-24 02:37:36 +0100

    feat: use environment fallbacks for flags

--- pkg/arguments/arguments.go
+++ pkg/arguments/arguments.go
@@ -2,19 +2,25 @@ package arguments
 
 import (
 	"flag"
+	"fmt"
+	"os"
 )
 
-const DEFAULT_FILE_NAME = "shmux.conf"
+const DEFAULT_CONFIGURATION = "shmux.sh"
+const CONFIGURATION_ENVIRONMENT = "SHMUX_CONFIG"
 const DEFAULT_SHELL = "/bin/sh"
+const SHELL_ENVIRONMENT = "SHMUX_SHELL"
 
 const ARGUMENT_SEPARATOR = "--"
 
 func Parse() (shell string, config string, scriptName string, arguments []string) {
-	shell = *flag.String("shell", DEFAULT_SHELL, "Shell to be used to run the scripts")
-	config = *flag.String("config", DEFAULT_FILE_NAME, "Configuration file")
+	shellFlag := flag.String("s", "", fmt.Sprintf("Shell to be used to run the scripts (default %s)", DEFAULT_SHELL))
+	configFlag := flag.String("c", "", fmt.Sprintf("Configuration file path (default %s)", DEFAULT_CONFIGURATION))
 
 	flag.Parse()
 
+	shell = oneOf(*shellFlag, os.Getenv(SHELL_ENVIRONMENT), DEFAULT_SHELL)
+	config = oneOf(*configFlag, os.Getenv(CONFIGURATION_ENVIRONMENT), DEFAULT_CONFIGURATION)
 	scriptName = flag.Arg(0)
 
 	doubleDashIndex := index(flag.Args(), ARGUMENT_SEPARATOR)
@@ -36,4 +42,14 @@ func index(slice []string, element string) int {
 	}
 
 	return -1
+}
+
+func oneOf(items ...string) string {
+	for _, i := range items {
+		if i != "" {
+			return i
+		}
+	}
+
+	return ""
 }
